ef376966a62a45aedd3d5302d291eb22bc8a3e1f
Sergey Ivanov <ivanitch.mail@gmail.com>
2021-03-30 01:10:15 +0900

    Added currency table

---
+++ console/migrations/m210329_160840_create_currency_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%currency}}`.
+ */
+class m210329_160840_create_currency_table extends Migration
+{
+    public function safeUp()
+    {
+        $this->createTable('{{%currency}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(3)->notNull(),
+            'rate' => $this->decimal(11, 4)->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%currency}}');
+    }
+}
